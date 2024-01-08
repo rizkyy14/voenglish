b8802549ce25baa94c3d0aa09c11d835722de480
rizkyy14 <133234016+rizkyy14@users.noreply.github.com>
2024-01-08 09:06:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // JavaScript
 const navbar = document.querySelector('nav');
-const totop = document.querySelector('.totop');
 
 window.onscroll = () => {
   if (window.scrollY > 90) {
@@ -36,3 +35,26 @@ const formatMessage = (obj) => {
   Alamat : ${obj.address}
   Saya ingin mengetahui lebih lanjut tentang program les ini, terima kasih`;
 };
+
+//Get the button
+let mybutton = document.getElementById('btn-back-to-top');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener('click', backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
